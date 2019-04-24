632b5141be13bc752f007e485eff1c2920066e5d
Ninad <mohiteninad15@gmail.com>
2019-04-24 17:58:39 +0530

    INITIAL COMMIT

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-toolbar app class="nav-bar">
+      <v-toolbar-title class="title">
+        <span class="title">LEONARDO</span>
+      </v-toolbar-title>
+ 
+      <!-- <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn> -->
+    </v-toolbar>
+
+    <v-content>
+      <v-layout style="background: rgba(0,0,0,0.8);">
+        <Grid/>
+      </v-layout>
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+import Grid from './components/Grid.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Grid
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .nav-bar{
+    background: #000;
+    color: crimson
+  }
+
+  .title{
+    font-size: 32pt;
+    font-weight: bold;
+    float: center;
+    justify-content: center;
+  }
+</style>
+
---
+++ src/components/Grid.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-container fluid v-bind="{ [`grid-list-${size}`]: true }" >
+
+    <v-layout row wrap>
+            
+      <v-flex v-for="n in 36" :key="n" xs3 style="padding: 8px;">
+
+        <v-hover>
+
+          <v-card tile slot-scope="{ hover }">
+
+            <v-img
+                :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                :aspect-ratio="1">
+              
+              <v-layout justify-center>
+
+                <v-btn flat color= "#fff" class="my-span">$120</v-btn>                
+               
+                <v-fade-transition >
+                  <div v-if="hover">
+                    <v-btn dark color="primary" @click.stop="dialog = true" >BUY</v-btn>     
+                    <v-dialog v-model="dialog" max-width = "500">
+                      <v-card>
+                        <v-card-title class="headline">Enter your key</v-card-title>
+                        <v-card-text>
+                          <v-text-field ></v-text-field>
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn>
+                          <v-btn color="blue darken-1" flat @click="dialog = false">Buy</v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>                  
+                  </div>                  
+                </v-fade-transition>     
+
+                             
+                
+              </v-layout>                                          
+
+            </v-img>
+
+          </v-card>
+        </v-hover>     
+
+      </v-flex>
+
+    </v-layout>
+
+  </v-container>
+</template>
+
+<style scoped>
+  .my-span {
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 8px;
+    color: white;
+    font-weight: bold;
+    margin: 8px;
+    float: right;
+  }
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      size: 'sm',
+      items: [
+        { text: 'Extra small (2px)', value: 'xs' },
+        { text: 'Small (4px)', value: 'sm' },
+        { text: 'Medium (8px)', value: 'md' },
+        { text: 'Large (16px)', value: 'lg' },
+        { text: 'Extra large (24px)', value: 'xl' }
+      ],
+      dialog : false
+    })
+    
+  }
+</script>
+
+
