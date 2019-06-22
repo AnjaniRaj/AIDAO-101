aa2f40fb85b0526bc53ff1a94e5ab0c257e6c111
AnjaniRaj <anjani.madhu97@gmail.com>
2019-06-22 19:22:54 +0530

    installed vuex

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import { VSlideXReverseTransition } from 'vuetify/lib';
+
+
+//Load Vuex
+Vue.use(Vuex);
+
+//create store
+export default new Vuex.Store({
+    moudules:{
+
+    }
+});
+
